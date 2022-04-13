f4fc2d0e5c643bc9789a880893a02733de8f6c43
Marine M <malomclt@gmail.com>
2022-04-13 14:52:36 +0200

    import createstacknav

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaContext } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  console.log('Stack', Stack);
+
   return (
        <NavigationContainer>{
        <View>
